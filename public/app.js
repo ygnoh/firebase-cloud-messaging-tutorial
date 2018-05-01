efe0762af260626a9ba17e232c0008af6cf51a16
yonggoonoh <nohyg90@gmail.com>
2018-05-01 23:41:29 +0900

    customize basic firebase

---
+++ public/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyBYZa9JdF_mmkUf7Yo1lWJm9w7YQ5ULr2s",
+    authDomain: "tutorial-69a13.firebaseapp.com",
+    databaseURL: "https://tutorial-69a13.firebaseio.com",
+    projectId: "tutorial-69a13",
+    storageBucket: "",
+    messagingSenderId: "927229004284"
+};
+firebase.initializeApp(config);
+
+console.log("loaded");
